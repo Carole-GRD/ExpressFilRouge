2da4e0b49e7b8948aa5e4a3f9bf1fc38c94b14ad
Carole Gérard <gerard.carole@skynet.be>
2022-07-27 16:31:11 +0200

    task-router.js + task-validator.js

--- routes/task-router.js
+++ routes/task-router.js
@@ -1,16 +1,18 @@
 const taskController = require('../controllers/task-controller');
-
+const bodyValidation = require('../middlewares/body-validation');
+const idValidator = require('../middlewares/idValidator');
+const { insertTaskValidator, updateTaskValidator } = require('../validators/task-validator');
 
 const taskRouter = require('express').Router();
 
 taskRouter.route('/')
     .get(taskController.getAll)
-    .post(taskController.create);
+    .post(bodyValidation(insertTaskValidator), taskController.create);
 
 taskRouter.route('/:id')
-    .get(taskController.getById)
-    .put(taskController.update)
-    .delete(taskController.delete);
+    .get(idValidator(), taskController.getById)
+    .put(idValidator(), bodyValidation(updateTaskValidator), taskController.update)
+    .delete(idValidator(), taskController.delete);
 
 taskRouter.route('/category/:categoryname')
     .get(taskController.getByCategory);
